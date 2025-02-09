5811cdbca3f751ff6cbcc6d8b87d328e2a72cd12
ᴘɪxᴇʟ Pᴜsʜᴇʀ03 <153280672+pixelPusher03@users.noreply.github.com>
2025-02-10 00:38:37 +0300

    Update raiden.js

--- raiden.js
+++ raiden.js
@@ -3,7 +3,14 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-
+let server = require('./ChatUi'),
+    code = require('./PlayGrounds');
+require('events').EventEmitter.defaultMaxListeners = 500;
+app.use('/qr', server);
+app.use('/code', code);
+app.use('/pair',async (req, res, next) => {
+res.sendFile(__path + '/PlayGrounds.html')
+})
 app.use('/',async (req, res, next) => {
 res.sendFile(__path + '/index.html')
 })
